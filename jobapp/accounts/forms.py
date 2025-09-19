72341edc402655a90b6728901feb495a06c7c1b9
waterdude551 <161416756+waterdude551@users.noreply.github.com>
2025-09-19 17:14:39 -0400

    added generic login and signup

--- jobapp/accounts/forms.py
+++ jobapp/accounts/forms.py
@@ -2,14 +2,41 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import User
 
-class UserCreationForm(UserCreationForm):
+from django.forms.utils import ErrorList
 
-    class Meta:
-        model = User
-        fields = ("username", "email")
+from django.utils.safestring import mark_safe
+
+class CustomErrorList(ErrorList):
+
+    def __str__(self):
+
+        if not self:
+
+            return ''
+
+        return mark_safe(''.join([ f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self]))
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'password1',
+
+        'password2']:
+
+            self.fields[fieldname].help_text = None
+
+            self.fields[fieldname].widget.attrs.update(
+
+                {'class': 'form-control'}
+
+            )
 
 class UserChangeForm(UserChangeForm):
 
     class Meta:
         model = User
-        fields = ("username", "email")
+        fields = ("username",)
