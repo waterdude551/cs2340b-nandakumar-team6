0ba7c45648414d84faca3c0de841394eab6eb1b8
catherine <carrietu101@gmail.com>
2025-09-19 19:52:41 -0400

    Templates/jobposting
    added above

--- jobapp/jobposting/views.py
+++ jobapp/jobposting/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+"""
+GOALS:
+    - should have a model called job post
+    - should have a url that displays all job posting
+    - should have a url that displays stuff for adding a job
+
+TO DO:
+    - double check 
+"""
+
 # Create your views here.
+def browsing(request):
+    #need to get all job postings
+    #then display them
+    return render(request, 'jobposting/browsing.html') #TODO
+
+def addpost(request):
+    return render(request, 'jobposting/addpost.html') #TODO
