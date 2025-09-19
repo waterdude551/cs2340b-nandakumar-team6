40764821b713a59776982ab7652a02cc5565b40e
catherine <carrietu101@gmail.com>
2025-09-19 19:39:24 -0400

    Created jobapp
    Goal: should browse and add job posting

---
+++ jobapp/jobposting/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('browsing/', views.signup, name='jobposting.browsing'),
+    path('addpost/', views.login, name='jobposting.addpost'),
+    #path('/', views.logout, name='jobposting.url3'),
+
+]
