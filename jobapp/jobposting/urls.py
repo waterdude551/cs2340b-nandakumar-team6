88a2cc0def635b27df4643876d310e8b67bc6cf0
Aliceriee <149611743+Aliceriee@users.noreply.github.com>
2025-09-19 20:40:46 -0400

    our website might be formatted
    tentative

--- jobapp/jobposting/urls.py
+++ jobapp/jobposting/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('browsing/', views.signup, name='jobposting.browsing'),
     path('addpost/', views.login, name='jobposting.addpost'),
+    path('addpost/', views.addpost, name='addpost'),
     #path('/', views.logout, name='jobposting.url3'),
 
 ]
