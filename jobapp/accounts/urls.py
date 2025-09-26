cf5c4da064afda6c71bb99324202ebdb1dfda372
Neal Cronin <93092110+NealCronin@users.noreply.github.com>
2025-09-26 04:12:08 -0400

    added seeker search

--- jobapp/accounts/urls.py
+++ jobapp/accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.login, name='accounts.login'),
     path('logout/', views.logout, name='accounts.logout'),
     path('profile/<int:id>/', views.profile, name='accounts.profile'),
+    path('search/', views.search_users, name='accounts.search'),
     path('edit-profile/<int:id>/', views.edit_profile, name='accounts.edit_profile'),
 ]
