4ed7ad2e24648f4957b5a7beeb54641ae572b8b8
ctu49 <ctu49@gatech.edu>
2025-10-24 13:42:46 +0000

    updating origin to be in line with deployed ver, changed way to get user location bc it wasnt working in python anywherei i:

--- jobapp/jobapp/settings.py
+++ jobapp/jobapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-kv(9v#fyl821nf=+hhh1l9&i*t-^0ww-4jpji(m$b#9hlk62x5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
